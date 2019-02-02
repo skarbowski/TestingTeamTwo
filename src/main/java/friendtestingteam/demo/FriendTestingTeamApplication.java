748abe365738496c4f069e453b7ac811a2651ae1
skarbowski <46647125+skarbowski@users.noreply.github.com>
2019-02-02 19:04:45 +0100

    First Commit

---
+++ src/main/java/friendtestingteam/demo/FriendTestingTeamApplication.java
@@ -0,0 +1,14 @@
+package friendtestingteam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendTestingTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendTestingTeamApplication.class, args);
+    }
+
+}
+

f61337b3eb08e0a1341e53da182b37b5eaef4cd6
skarbowski <46647125+skarbowski@users.noreply.github.com>
2019-02-02 20:56:02 +0100

    Second Commit

--- src/main/java/TestThisAndThat.java
+++ src/main/java/TestThisAndThat.java
@@ -1,2 +1,18 @@
 public class TestThisAndThat {
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+
+    public double substractAFromB(double a, double b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+
+        TestThisAndThat Calculator = new TestThisAndThat();
+
+        double result = Calculator.addAToB(2.5, 2.45);
+        System.out.println("Its Work and result is : " + result);
+
+    }
 }
--- src/main/java/friendtestingteam/demo/FriendTestingTeamApplication.java
+++ src/main/java/friendtestingteam/demo/FriendTestingTeamApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FriendTestingTeamApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FriendTestingTeamApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FriendTestingTeamApplication.class, args);
+	}
 
 }
 
